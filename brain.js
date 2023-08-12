a6f0e4a3f3f0210add4a057cf4ed082c28406e87
siwangigahlot <133335366+siwangigahlot@users.noreply.github.com>
2023-08-12 17:13:44 +0530

    Add files via upload

---
+++ brain.js
@@ -0,0 +1,17 @@
+
+var i = prompt(enter either 0 or 1:);
+
+let a = getelementbyid(number).value;
+
+function convert(a){
+    if (i=1) {
+        
+        let celsius= (a-32)*5/9;
+        console.log(celsius);
+    }
+else if(i=0)
+{
+    let fahrenheit = (a*9/5)+32;
+    console.log(fahrenheit);
+}
+}
